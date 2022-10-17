25d80097717c88bda6f432a787d1697eab7b3e37
Natalia Venkatesh <nataliavenkatesh@macbook-pro.lan>
2022-10-17 12:45:02 -0400

    fetch funciton

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,15 @@ var searchButton = document.getElementById('searchButton');
 
 // const APIKey = "79fcf7dc6a392bfb9db10a054d9e49c5";
 
-var city = document.getElementById('citySearch').value;
-console.log("city")
+// var city = document.getElementById('citySearch').value;
+// console.log("city")
 // var state;
 // var country;
 
 
 function getApi(){
-
-   var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=city&limit=5&appid=79fcf7dc6a392bfb9db10a054d9e49c5"
+   var city = document.getElementById('citySearch');
+   var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=79fcf7dc6a392bfb9db10a054d9e49c5'
 
 
     fetch(requestUrl)
@@ -22,11 +22,18 @@ function getApi(){
         return response.json();
         })
         .then(function (data) {
-            for (var i=0; i<data.legth; i++){
-            console.log(data[i]);
-        }
-    });
+        
+        console.log(data);
+    }); 
+   
 }
+
+// function fetchWeather(location){
+//     console.log(location)
+//     var  {lat}=location;
+//     var {log} =location;
+
+
 // function getApi5(){
 //     var requestUrl5 = "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=79fcf7dc6a392bfb9db10a054d9e49c5"
 //         fetch(requestUrl)
